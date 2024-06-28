432d903bec182aee015e8a0d7892175046995b93
libamts <libamtr@liba.com.br>
2024-06-28 17:48:05 -0300

    Ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
-    for(let i = 0; 1 < buttons.length; i++){
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const posiçãoAtual = window.scrollY;
+
+        if (posiçãoAtual < alturaHero){
+            ocultaElementosDoHeader();
+        }else{
+            exibeElementosDoHeader();
+        }
+    })
+
+    //secão de atrações, programação das abas
+    for(let i = 0; i < buttons.length; i++){
+
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
@@ -9,15 +25,28 @@ document.addEventListener('DOMContentLoaded', function(){
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
-            // removeBotaoAtivo();
+            removeBotaoAtivo();
         })
     }
+
+    //seção FAQ, accordion
+    for(let i = 0; i < questions.length; i++ ) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function abreOuFechaResposta(elemento){
+    console.log(elemento);
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+    
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
 
-    for(let i = 0; 1 < buttons.length; i++){
+    for(let i = 0; i < buttons.length; i++){
         buttons[i].classList.remove('shows__tabs__button--is-active')
     }
 }
@@ -29,3 +58,14 @@ function escondeTodasAbas(){
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
+}
+
